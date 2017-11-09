299e7ad635a706779cda896ab3dd38aee634b179
Omar Esai Vazquez <ovazquez@eng.ucsd.edu>
2017-11-08 23:01:16 -0800

    Add files via upload

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Shroom', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Anything Goes', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Deluxe', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Vegan', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Any BurgerYour Way', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Healthy Deluxe', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Crab Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Top Burger Extra Cheese', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Bottomless Burger', false);
